90da3bcccf76ae823363225603c5fdf6b695241a
MatteoDiMario96 <dm.matteo96@live.it>
2024-11-14 01:39:06 +0100

    Put two components Button and an h1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Button from './assets/Button.jsx';
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Button />
+      <Button />
+    </>
+  )
+}
+
+export default App
